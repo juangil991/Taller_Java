f485b5dc83d9c998bb7fe25fe791b2292f5038db
juangil991 <jcgo125@gmail.com>
2022-02-14 20:59:33 -0500

    se añaden comentarios finales

--- src/com/sofka/taller/Main.java
+++ src/com/sofka/taller/Main.java
@@ -4,21 +4,37 @@ public class Main {
 
     public static void main(String[] args) {
         Funciones calculo = new Funciones();
+        //punto 1 y 2
         //System.out.println(calculo.calculoMayor_Menor());
+        //punto 3
         //System.out.println(calculo.calculoAreaCirculo());
+        //punto 4
         //System.out.println(calculo.calculoIva());
+        //punto 5 y 6
         //calculo.numerosImpares();
+        //punto 7
         //calculo.mayorCero();
+        //punto 8
         //calculo.diaLaboral();
+        //punto 9
         //calculo.reemplazarChar();
+        //punto 10
         //calculo.eliminarChar();
+        //punto 11
         //calculo.contadorVocales();
+        //punto 12
         //calculo.stringDiferencias();
+        //punto 13
         //calculo.fechaActual();
+        //punto 14
         //calculo.Saltos();
+        //punto 15
         //calculo.Menu();
+        //punto 16
         //PersonaEjecutable personas= new PersonaEjecutable();
+        //punto 17
         //ElectrodomesticosEjecutables allelectrodomesticos=new ElectrodomesticosEjecutables();
+        //punto 18
         //VideojuegoEjecutable Games = new VideojuegoEjecutable();
         //SerieEjecutable Series = new SerieEjecutable();
     }
